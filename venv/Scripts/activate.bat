84da4962fffca4eac2487e0ae392bd5079c20434
jorgezb <jorgezapata990213239@gmail.com>
2025-03-10 19:53:46 -0300

    Primer commit

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\lacho\mi_proyecto\venv
+set VIRTUAL_ENV=C:\Users\ALUMNO\Desktop\mi_proyecto\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
